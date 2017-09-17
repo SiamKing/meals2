cf7ba3a78f3a408fa6bcc7cb5b93d4997e95c4e8
SiamKing <thesiameseelectric@gmail.com>
2017-09-17 16:16:44 -0700

    created user_meal table and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170917224713) do
+ActiveRecord::Schema.define(version: 20170917230209) do
 
   create_table "meals", force: :cascade do |t|
     t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_meals", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "meal_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
